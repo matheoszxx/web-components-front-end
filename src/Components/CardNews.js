68a63d66d5a68db3a2eeb9292f7fb1e5b56ef41e
matheus <matheuszxx1@outlook.com>
2023-09-25 15:53:20 -0300

    construindo o 'build' e as 'Props' do component

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -5,7 +5,7 @@ class CardNews extends HTMLElement {
 
         const shadow = this.attachShadow({ mode: "open" });
         shadow.appendChild(this.build())
-        shadow.appendChild(this.styles)
+        shadow.appendChild(this.styles())
 
     }
 
@@ -17,8 +17,26 @@ class CardNews extends HTMLElement {
         const cardLeft = document.createElement("div")
         cardLeft.setAttribute("class", "card_left")
 
+        const autor = document.createElement("span")
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous")
+
+        const linkTitle = document.createElement("a")
+        linkTitle.textContent = this.getAttribute("title")
+
+        const newsContent = document.createElement("p")
+        newsContent.textContent = this.getAttribute("content")
+
+        cardLeft.appendChild(autor)
+        cardLeft.appendChild(linkTitle)
+        cardLeft.appendChild(newsContent)
+
+
         const cardRight = document.createElement("div")
         cardRight.setAttribute("class", "card_right")
+
+        const newsImage = document.createElement("img")
+
+        cardRight.appendChild(newsImage)
 
         //são filhas do componentRoot que no HTML é o "card"
         componentRoot.appendChild(cardLeft)
